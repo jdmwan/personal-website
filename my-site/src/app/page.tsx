cd861e28ff7b268abfd524255264c9ad03cc2933
jeffrey <jeffrey@jeffreys-MacBook-Pro.local>
2025-08-05 14:33:16 -0400

    fixed quotation marks

--- my-site/src/app/page.tsx
+++ my-site/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             Many factors influence strength.
           </h1>
           <h1 className="text-4xl sm:text-5xl font-semibold text-zinc-100">
-            But Victory Never lies."
+            But Victory Never lies.“
           </h1>
           <p className="text-sm text-zinc-400 italic">– Mavuika</p>
           <p className="text-sm text-gray-400 animate-bounce mt-8">Scroll down to learn more ↓</p>
@@ -31,7 +31,7 @@ export default function Home() {
         <div className="space-y-4">
           <h2 className="text-3xl font-bold">Hi, I’m Jeffrey Wan</h2>
           <p className="text-zinc-300">
-            Welcome to my webpage. You'll see lots of fun projects scattered around, and learn about me bit by bit. Go to the resume page to see what I do, and scroll down for more info about me.
+            Welcome to my webpage. You’ll see lots of fun projects scattered around, and learn about me bit by bit. Go to the resume page to see what I do, and scroll down for more info about me.
           </p>
           <p className="text-sm text-gray-400 animate-bounce mt-8">Scroll down to learn more ↓</p>
         </div>
